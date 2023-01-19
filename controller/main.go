6c6d31713c5789400e659a5eb22e10217f5e792c
aleksandrzhiliaev <aleksandr.zhiliaev@goflink.com>
2023-01-19 18:18:08 +0100

    add reatcor logic

--- controller/main.go
+++ controller/main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/url"
 	"path/filepath"
+	"regexp"
 	"time"
 
 	"github.com/gorilla/websocket"
@@ -16,7 +18,7 @@ import (
 )
 
 var (
-	minecraftSocketAddress = "116.202.8.204:4567"
+	minecraftSocketAddress = "116.202.8.204:3567"
 	minecraftSocketPath    = "/v1/ws/console"
 )
 
@@ -31,14 +33,27 @@ func main() {
 		panic(err)
 	}
 
+	// react on minecraft events in separate goroutine
+	go func() {
+		for {
+			err = kubeReactor(clientset)
+			if err != nil {
+				log.Println("kubeReactor error:", err)
+			}
+		}
+	}()
+
+	//observe k8s pods and namespaces in main goroutine
 	for {
-		kubeObserver(clientset)
+		err = kubeObserver(clientset)
+		if err != nil {
+			log.Println("kubeObserver error:", err)
+		}
 		time.Sleep(1 * time.Minute)
 	}
-
 }
 
-func kubeObserver(clientset *kubernetes.Clientset) {
+func kubeObserver(clientset *kubernetes.Clientset) error {
 	// Idea:
 	// 1. Get labeled ns
 	// 2. Get pods from namespace
@@ -47,7 +62,7 @@ func kubeObserver(clientset *kubernetes.Clientset) {
 		LabelSelector: "flinktoid=true",
 	})
 	if err != nil {
-		panic(err)
+		return fmt.Errorf("failed to list namespaces: %w", err)
 	}
 
 	// init websocket connection begin
@@ -55,29 +70,112 @@ func kubeObserver(clientset *kubernetes.Clientset) {
 	log.Printf("connecting to %s", u.String())
 	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
 	if err != nil {
-		log.Fatal("dial:", err)
+		return fmt.Errorf("failed to dial websocket : %w", err)
+	}
+	defer c.Close()
+
+	// cleanup world first
+	err = c.WriteMessage(websocket.TextMessage, []byte("/kill @e[type=item]"))
+	if err != nil {
+		return fmt.Errorf("failed to send kill command to minecraft: %w", err)
 	}
 
 	for _, ns := range namespaces.Items {
-		pods, err := clientset.CoreV1().Pods(ns.Name).List(context.TODO(), metav1.ListOptions{})
+		pods, err := clientset.CoreV1().Pods(ns.Name).List(context.Background(), metav1.ListOptions{})
 		if err != nil {
-			panic(err)
+			return fmt.Errorf("failed to list pods: %w", err)
 		}
 
-		fmt.Println("Namespace:", ns.Name)
-
 		for _, pod := range pods.Items {
-			fmt.Println("Pod:", pod.Name)
-
 			// summon reference: https://minecraft.fandom.com/wiki/Commands/summon
 			err = c.WriteMessage(websocket.TextMessage, []byte(`/summon pig 94 64 -44 {CustomName:"\"`+pod.Name+`\"",CustomNameVisible:1}`))
 			if err != nil {
-				log.Println("write for this pod:", pod.Name, err)
-				return
+				return fmt.Errorf("failed to send summon command to minecraft: %w", err)
 			}
 		}
 	}
+	return nil
+}
 
-	// loop end, close websocket connection
-	c.Close()
+func kubeReactor(clientset *kubernetes.Clientset) error {
+	// Idea:
+	// 1. Watch websocket from minecraft server
+	// 2. Parse message and do some action
+
+	// init websocket connection begin
+	u := url.URL{Scheme: "ws", Host: minecraftSocketAddress, Path: minecraftSocketPath}
+	log.Printf("connecting to %s", u.String())
+	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+	if err != nil {
+		return fmt.Errorf("failed to dial websocket : %w", err)
+	}
+
+	// watch websocket from minecraft server
+	log.Println("watching websocket events...")
+	for {
+		_, message, err := c.ReadMessage()
+		if err != nil {
+			return fmt.Errorf("failed to read message from websocket: %w", err)
+		}
+
+		//message := []byte(`{"message": "Named entity EntityCow['default_nginx'/374, uuid='71e6341b-4667-4100-bc91-7e7825078df3', l='ServerLevel[world]', x=8.64, y=86.00, z=7.96, cpos=[0, 0], tl=51, v=true] died: default_nginx was slain by AranelSurion", "timestampMillis": 1631834015918, "loggerName": "", "level": "INFO"}`)
+		ns, pod, err := parseMinecraftUserKillMessage(message)
+		if err != nil {
+			log.Println("failed to parse minecraft message:", err)
+			continue
+		}
+		if ns != "" {
+			log.Println("parsed ns:", ns, "pod:", pod)
+			// call k8s to delete pod in ns
+			err = clientset.CoreV1().Pods(ns).Delete(context.Background(), pod, metav1.DeleteOptions{})
+			if err != nil {
+				log.Println("failed to delete pod:", err)
+				continue
+			}
+			log.Println("deleted pod:", pod)
+		}
+	}
+
+	defer c.Close()
+	return nil
+}
+
+func parseMinecraftUserKillMessage(body []byte) (string, string, error) {
+	// Idea: Parse message and handle it:
+	// Example: {"message": "Named entity EntityCow['default_nginx'/374, uuid='71e6341b-4667-4100-bc91-7e7825078df3', l='ServerLevel[world]', x=8.64, y=86.00, z=7.96, cpos=[0, 0], tl=51, v=true] died: default_nginx was slain by AranelSurion", "timestampMillis": 1631834015918, "loggerName": "", "level": "INFO"}
+
+	// 1. Parse message
+	type MinecraftMessage struct {
+		Message         string `json:"message"`
+		TimestampMillis int64  `json:"timestampMillis"`
+		LoggerName      string `json:"loggerName"`
+		Level           string `json:"level"`
+	}
+	var message = MinecraftMessage{}
+
+	err := json.Unmarshal(body, &message)
+	if err != nil {
+		log.Println("failed to parse message:", err)
+		return "", "", err
+	}
+	if message.Level != "INFO" {
+		return "", "", nil
+	}
+
+	re := regexp.MustCompile(`(?P<ns>[a-z-0-9]+)_(?P<pod>[a-z-0-9]+) was slain by`)
+	if err != nil {
+		log.Println("failed to compile regexp:", err)
+		return "", "", err
+	}
+
+	// get pod, namespace
+	matches := re.FindStringSubmatch(message.Message)
+	if len(matches) != 3 {
+		log.Println("message find player and entity mismatch, skipping:", message.Message)
+		return "", "", nil
+	}
+	namespace := matches[1]
+	pod := matches[2]
+
+	return namespace, pod, nil
 }
